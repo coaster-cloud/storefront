3e0d94e968fce801b48e428f3a1128b25bb2db98
Michel Chowanski <michel@chowanski.de>
2021-02-10 22:42:15 +0100

    Try new profile style

--- pages/profile/_username/index.vue
+++ pages/profile/_username/index.vue
@@ -48,11 +48,11 @@
         </b-col>
       </b-row>
 
-      <b-row>
-        <b-col md="8">
-          <!-- Heatmap -->
+      <b-row class="mb-5">
+        <!-- Heatmap -->
+        <b-col offset-md="2" md="8">
           <client-only>
-            <div class="content-block">
+            <div class="text-center">
               <h5>{{ $t('heatmap') }}</h5>
               <p>{{ $t('last_count_activities') }}</p>
 
@@ -65,125 +65,140 @@
               />
             </div>
           </client-only>
+        </b-col>
+      </b-row>
 
-          <b-row>
-            <!-- Attractions -->
-            <b-col lg="6">
-              <div class="content-block">
-                <h5>{{ $t('attractions') }}</h5>
-                <p>{{ $t('counted_attractions') }}</p>
-
-                <template v-if="account.rideStatistic.attractionRides.items.length === 0">
-                  <no-data />
-                </template>
-
-                <template v-else>
-                  <b-list-group flush>
-                    <template v-for="(attractionRide, index) in account.rideStatistic.attractionRides.items">
-                      <b-list-group-item :key="index" class="flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                          <nuxt-link class="text-truncate" :to="localePath({name: 'attractions-attraction', params: {attraction: attractionRide.attraction.fullSlug}})">
-                            {{ attractionRide.attraction.name }}
-                          </nuxt-link>
-                        </div>
-
-                        <small>{{ $tc('n_rides', attractionRide.rides, {'count': attractionRide.rides}) }}</small>
-                      </b-list-group-item>
-                    </template>
-                  </b-list-group>
-
-                  <b-pagination
-                    v-if="account.rideStatistic.attractionRides.pagination.totalItems > itemsPerPage"
-                    v-model="attractionPage"
-                    :limit="3"
-                    align="center"
-                    class="align-items-center"
-                    :total-rows="account.rideStatistic.attractionRides.pagination.totalItems"
-                    :per-page="itemsPerPage"
-                  />
-                </template>
-              </div>
-            </b-col>
-
-            <!-- Parks -->
-            <b-col lg="6">
-              <div class="content-block">
-                <h5>{{ $t('parks') }}</h5>
-                <p>{{ $t('counted_parks') }}</p>
-
-                <template v-if="account.rideStatistic.parkVisits.items.length === 0">
-                  <no-data />
-                </template>
-
-                <template v-else>
-                  <b-list-group flush>
-                    <template v-for="(parkVisits, index) in account.rideStatistic.parkVisits.items">
-                      <b-list-group-item :key="index" class="flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                          <nuxt-link class="text-truncate" :to="localePath({name: 'parks-park', params: {park: parkVisits.park.fullSlug}})">
-                            {{ parkVisits.park.name }}
-                          </nuxt-link>
-                        </div>
-
-                        <small>{{ $tc('n_visits', parkVisits.visits, {'count': parkVisits.visits}) }}</small>
-                      </b-list-group-item>
-                    </template>
-                  </b-list-group>
-
-                  <b-pagination
-                    v-if="account.rideStatistic.parkVisits.pagination.totalItems > itemsPerPage"
-                    v-model="parkPage"
-                    :limit="3"
-                    align="center"
-                    class="align-items-center"
-                    :total-rows="account.rideStatistic.parkVisits.pagination.totalItems"
-                    :per-page="itemsPerPage"
-                  />
-                </template>
-              </div>
-            </b-col>
-
-            <!-- Manufacturers -->
-            <b-col lg="6">
-              <div class="content-block">
-                <h5>{{ $t('manufacturers') }}</h5>
-                <p>{{ $t('counted_manufacturers') }}</p>
-
-                <template v-if="account.rideStatistic.parkVisits.items.length === 0">
-                  <no-data />
-                </template>
-
-                <template v-else>
-                  <b-list-group flush>
-                    <template v-for="(manufacturerRide, index) in account.rideStatistic.manufacturerRides.items">
-                      <b-list-group-item :key="index" class="flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                          <nuxt-link class="text-truncate" :to="localePath({ name: 'attractions', query: { manufacturer: manufacturerRide.manufacturer.id } })">
-                            {{ manufacturerRide.manufacturer.name }}
-                          </nuxt-link>
-                        </div>
-
-                        <small>{{ $tc('n_rides', manufacturerRide.totalRides, {'count': manufacturerRide.totalRides}) }}</small>
-                      </b-list-group-item>
-                    </template>
-                  </b-list-group>
-
-                  <b-pagination
-                    v-if="account.rideStatistic.manufacturerRides.pagination.totalItems > itemsPerPage"
-                    v-model="manufacturerPage"
-                    :limit="3"
-                    align="center"
-                    class="align-items-center"
-                    :total-rows="account.rideStatistic.manufacturerRides.pagination.totalItems"
-                    :per-page="itemsPerPage"
-                  />
-                </template>
-              </div>
-            </b-col>
-          </b-row>
+      <b-row class="mb-5">
+        <b-col cols="12">
+          <div class="text-center">
+            <h5>{{ $t('ride_facts') }}</h5>
+          </div>
         </b-col>
 
-        <b-col md="4" class="left-separator" />
+        <b-col v-for="(fact, index) in account.rideStatistic.summary" :key="index" lg="3" md="4" sm="6">
+          <div class="text-center mt-4">
+            <p class="mb-1">
+              {{ fact.label }}
+            </p>
+            <span class="text-muted">{{ fact.value }}</span>
+          </div>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <!-- Attractions -->
+        <b-col lg="4">
+          <div class="content-block">
+            <h5>{{ $t('attractions') }}</h5>
+            <p>{{ $t('counted_attractions') }}</p>
+
+            <template v-if="account.rideStatistic.attractionRides.items.length === 0">
+              <no-data />
+            </template>
+
+            <template v-else>
+              <b-list-group flush>
+                <template v-for="(attractionRide, index) in account.rideStatistic.attractionRides.items">
+                  <b-list-group-item :key="index" class="flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <nuxt-link class="text-truncate" :to="localePath({name: 'attractions-attraction', params: {attraction: attractionRide.attraction.fullSlug}})">
+                        {{ attractionRide.attraction.name }}
+                      </nuxt-link>
+                    </div>
+
+                    <small>{{ $tc('n_rides', attractionRide.rides, {'count': attractionRide.rides}) }}</small>
+                  </b-list-group-item>
+                </template>
+              </b-list-group>
+
+              <b-pagination
+                v-if="account.rideStatistic.attractionRides.pagination.totalItems > itemsPerPage"
+                v-model="attractionPage"
+                :limit="3"
+                align="center"
+                class="align-items-center"
+                :total-rows="account.rideStatistic.attractionRides.pagination.totalItems"
+                :per-page="itemsPerPage"
+              />
+            </template>
+          </div>
+        </b-col>
+
+        <!-- Parks -->
+        <b-col lg="4">
+          <div class="content-block">
+            <h5>{{ $t('parks') }}</h5>
+            <p>{{ $t('counted_parks') }}</p>
+
+            <template v-if="account.rideStatistic.parkVisits.items.length === 0">
+              <no-data />
+            </template>
+
+            <template v-else>
+              <b-list-group flush>
+                <template v-for="(parkVisits, index) in account.rideStatistic.parkVisits.items">
+                  <b-list-group-item :key="index" class="flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <nuxt-link class="text-truncate" :to="localePath({name: 'parks-park', params: {park: parkVisits.park.fullSlug}})">
+                        {{ parkVisits.park.name }}
+                      </nuxt-link>
+                    </div>
+
+                    <small>{{ $tc('n_visits', parkVisits.visits, {'count': parkVisits.visits}) }}</small>
+                  </b-list-group-item>
+                </template>
+              </b-list-group>
+
+              <b-pagination
+                v-if="account.rideStatistic.parkVisits.pagination.totalItems > itemsPerPage"
+                v-model="parkPage"
+                :limit="3"
+                align="center"
+                class="align-items-center"
+                :total-rows="account.rideStatistic.parkVisits.pagination.totalItems"
+                :per-page="itemsPerPage"
+              />
+            </template>
+          </div>
+        </b-col>
+
+        <!-- Manufacturers -->
+        <b-col lg="4">
+          <div class="content-block">
+            <h5>{{ $t('manufacturers') }}</h5>
+            <p>{{ $t('counted_manufacturers') }}</p>
+
+            <template v-if="account.rideStatistic.parkVisits.items.length === 0">
+              <no-data />
+            </template>
+
+            <template v-else>
+              <b-list-group flush>
+                <template v-for="(manufacturerRide, index) in account.rideStatistic.manufacturerRides.items">
+                  <b-list-group-item :key="index" class="flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <nuxt-link class="text-truncate" :to="localePath({ name: 'attractions', query: { manufacturer: manufacturerRide.manufacturer.id } })">
+                        {{ manufacturerRide.manufacturer.name }}
+                      </nuxt-link>
+                    </div>
+
+                    <small>{{ $tc('n_rides', manufacturerRide.totalRides, {'count': manufacturerRide.totalRides}) }}</small>
+                  </b-list-group-item>
+                </template>
+              </b-list-group>
+
+              <b-pagination
+                v-if="account.rideStatistic.manufacturerRides.pagination.totalItems > itemsPerPage"
+                v-model="manufacturerPage"
+                :limit="3"
+                align="center"
+                class="align-items-center"
+                :total-rows="account.rideStatistic.manufacturerRides.pagination.totalItems"
+                :per-page="itemsPerPage"
+              />
+            </template>
+          </div>
+        </b-col>
       </b-row>
     </div>
   </div>
@@ -433,6 +448,7 @@ query ($username: String!, $locale: String!, $filter: RideStatisticFilter!, $ite
           totalUniqueRides
         }
       }
+      summary { label, value }
     }
   }
 }
