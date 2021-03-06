6309005feba4f3aa7476c8342d2edbd9bdf1c95f
Michel Chowanski <michel@chowanski.de>
2021-03-06 10:07:09 +0100

    Switch order at help page

--- pages/help/index.vue
+++ pages/help/index.vue
@@ -5,26 +5,6 @@
 
       <b-row>
         <b-col sm="8">
-          <markdown-text :text="$t('help.update_resource')" />
-        </b-col>
-
-        <b-col sm="4">
-          <img src="~/assets/images/help/update_resource.png" class="img-fluid">
-        </b-col>
-      </b-row>
-
-      <b-row class="mt-4">
-        <b-col sm="8">
-          <markdown-text :text="$t('help.create_resource')" />
-        </b-col>
-
-        <b-col sm="4">
-          <img src="~/assets/images/help/create_resource.png" class="img-fluid">
-        </b-col>
-      </b-row>
-
-      <b-row class="mt-4">
-        <b-col sm="8">
           <markdown-text :text="$t('help.count_attraction')" />
         </b-col>
 
@@ -40,6 +20,26 @@
 
         <b-col sm="4">
           <img src="~/assets/images/help/rate_attraction.png" class="img-fluid">
+        </b-col>
+      </b-row>
+
+      <b-row class="mt-4">
+        <b-col sm="8">
+          <markdown-text :text="$t('help.update_resource')" />
+        </b-col>
+
+        <b-col sm="4">
+          <img src="~/assets/images/help/update_resource.png" class="img-fluid">
+        </b-col>
+      </b-row>
+
+      <b-row class="mt-4">
+        <b-col sm="8">
+          <markdown-text :text="$t('help.create_resource')" />
+        </b-col>
+
+        <b-col sm="4">
+          <img src="~/assets/images/help/create_resource.png" class="img-fluid">
         </b-col>
       </b-row>
     </div>
