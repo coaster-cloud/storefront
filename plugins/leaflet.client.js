00e62b8c42e8691e262c019509bff3ef3b3e9371
Michel Chowanski <michel@chowanski.de>
2021-03-09 21:16:38 +0100

    Fix missing map marker

--- plugins/leaflet.client.js
+++ plugins/leaflet.client.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 
 import 'leaflet/dist/leaflet.css'
-import 'leaflet/dist/images/marker-icon.png'
-import 'leaflet/dist/images/marker-icon-2x.png'
-import 'leaflet/dist/images/marker-shadow.png'
 import Leaflet from 'leaflet'
 
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import LeafletMarkerCluster from 'leaflet.markercluster'
+
+delete Leaflet.Icon.Default.prototype._getIconUrl
+
+Leaflet.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
 
 Vue.prototype.$leaflet = Leaflet
 Vue.prototype.$leafletMarkerCluster = LeafletMarkerCluster
